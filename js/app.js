0a00bac2e216709f988fb5903f0c329abb525537
cristianq3 <cristianq3@gmail.com>
2023-04-07 20:23:18 -0300

    finalizada la logica

--- js/app.js
+++ js/app.js
@@ -3,12 +3,31 @@ Cuando el usuario adivine el numero mostrar un mensaje indicando al usuario que 
 
 // crear el numero aleatorio apartir de que se presiona el boton, creare una funcion que haga eso y la pongo en la propiedad onclick en el documento html.
 
+let btnGenerar = document.querySelector("#btnGenerar");
+let input = document.querySelector("#ingresoNRo");
+
+let btnAdivinar = document.querySelector("#adivinar");
+btnGenerar.addEventListener("click", generarNumero);
+
+let numeroAleatorio = 0;
+
+btnAdivinar.addEventListener("click", adivinarNumero);
+
 function generarNumero() {
-  let numeroAleatorio = parseInt(Math.random() * 99);
+  numeroAleatorio = parseInt(Math.random() * 100);
   console.log(numeroAleatorio);
-  return numeroAleatorio;
 }
+function adivinarNumero() {
+  let valorAComparar = parseInt(input.value);
+  console.log(valorAComparar);
+  console.log(numeroAleatorio);
 
-
-
-
+  if (valorAComparar === numeroAleatorio) {
+    console.log("son igueales");
+    alert("Felicitaciones Ganaste");
+  } else if (valorAComparar > numeroAleatorio) {
+    alert("El numero que elegiste es mayor, sigue intentando.");
+  } else if (valorAComparar < numeroAleatorio) {
+    alert("El numero que elegiste es menor, sigue intentando.");
+  }
+}
